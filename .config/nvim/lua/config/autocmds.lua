1edc830ca33120dbf4c86945257f68937e0bd57f
Claude Becker <becker@phys.ethz.ch>
2024-10-05 15:49:56 +0200

    nvim: disable markdown auto-format on save

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,13 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable markdown spellchecking, keep line wrap enabled
+-- Disable markdown autoformat and spellchecking, keep line wrap enabled
 -- https://github.com/LazyVim/LazyVim/discussions/392
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("lazyvim_user_markdown", { clear = true }),
   pattern = { "markdown" },
   callback = function()
+    vim.b.autoformat = false
     vim.opt_local.wrap = true
     vim.opt_local.spell = false
   end,
