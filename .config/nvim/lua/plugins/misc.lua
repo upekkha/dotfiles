958ebcf36d1e71812e9b4d245dde26a3d20edcdc
Claude Becker <becker@phys.ethz.ch>
2024-10-07 07:32:59 +0200

    nvim: tweak render-markdown settings

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -60,6 +60,24 @@ return {
     enabled = false,
   },
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+    opts = {
+      code = {
+        style = "normal",  -- disable lang icons
+      },
+      -- Multi-line quotes
+      quote = {
+        repeat_linebreak = true,
+      },
+      win_options = {
+        showbreak = { default = "", rendered = "  " },
+        breakindent = { default = false, rendered = true },
+        breakindentopt = { default = "", rendered = "" },
+      },
+    },
+  },
+  {
    "nvimdev/dashboard-nvim",
    -- Custom dashboard logo
    -- https://github.com/LazyVim/LazyVim/discussions/742
