ebb164c3f21ed40880fcb4cc4f0bbb1eb82cc95e
Claude Becker <becker@phys.ethz.ch>
2024-12-28 17:03:58 +0100

    neovim: enable custom snippets

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,10 +2,8 @@ return {
   {
     "saghen/blink.cmp",
     opts = function(_, opts)
-      -- Disable completion from buffer and snippets.
-      opts.sources.default = { "lsp", "path" }
-      -- Completion sources per filetype (coming with next release).
-      -- opts.sources.per_filetype = { markdown = { "lsp", "path" } }
+      -- Completion sources per filetype.
+      opts.sources.per_filetype = { markdown = { "lsp", "path", "snippets" } }
       -- Use <Tab> to select from completion menu.
       opts.keymap = { preset = "super-tab" }
     end,
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- Disable automatic insertion of closing characters (brackets, quotes, etc).
   { "echasnovski/mini.pairs", enabled = false },
+  -- Disable large collection of snippets to focus on custom snippets.
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
