e7a4bf5451e7980b4ecbaba443d7fc01a63c190f
Claude Becker <becker@phys.ethz.ch>
2025-01-06 11:04:35 +0100

    neovim: re-disable snippet completion

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
     "saghen/blink.cmp",
     opts = function(_, opts)
       -- Completion sources per filetype.
-      opts.sources.per_filetype = { markdown = { "lsp", "path", "snippets" } }
+      opts.sources.per_filetype = { markdown = { "lsp", "path" } }
       -- Use <Tab> to select from completion menu.
       opts.keymap = { preset = "super-tab" }
     end,
