22a28e613aad91336fe527789585bb3ae34b7247
Claude Becker <becker@phys.ethz.ch>
2025-01-21 13:40:34 +0100

    neovim: disable mini.trailspace in snacks dashboard

--- .config/nvim/lua/plugins/mini-trailspace.lua
+++ .config/nvim/lua/plugins/mini-trailspace.lua
@@ -4,6 +4,12 @@ return {
     -- Highlight trailing whitespace and clean with `MiniTrailspace.trim()`.
     config = function()
       require("mini.trailspace").setup()
+      -- Disable in snacks dashboard
+      -- Source: https://github.com/echasnovski/mini.nvim/issues/1395
+      local f = function(args)
+        vim.b[args.buf].minitrailspace_disable = true
+      end
+      vim.api.nvim_create_autocmd("User", { pattern = "SnacksDashboardOpened", callback = f })
     end,
   },
 }
