7c5c443ed2b9c81c3cb599a4b5e0b1f9500b0b9c
Claude Becker <becker@phys.ethz.ch>
2025-02-02 15:01:56 +0100

    neovim: switch from fzf to snacks picker

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   -- Disable large collection of snippets to focus on custom snippets.
   { "rafamadriz/friendly-snippets", enabled = false },
+  -- Disable fzf-lua in favor of snacks picker.
+  { "ibhagwan/fzf-lua", enabled = false },
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,24 @@ return {
     opts = {
       -- Disable indent guides
       indent = { enabled = false },
+      -- Fuzzy picker
+      picker = {
+        -- Show hidden files and follow symlinks
+        sources = {
+          files = { hidden = true, follow = true },
+          explorer = { hidden = true, follow = true },
+        },
+        -- Add macOS-compatible keymaps to toggle hidden and ignored files.
+        win = {
+          input = {
+            keys = {
+              ["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+              ["<c-i>"] = { "toggle_ignore", mode = { "i", "n" } },
+              ["<Esc>"] = { "close", mode = { "n", "i" } },
+            },
+          },
+        },
+      },
       -- Customize dashboard
       dashboard = {
         preset = {
