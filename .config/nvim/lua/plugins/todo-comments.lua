d9407b6804bde2fa6fa8da3797531b99b04613cf
Claude Becker <becker@phys.ethz.ch>
2025-02-03 10:39:39 +0100

    neovim: highlight keywords only with colon

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -3,11 +3,11 @@ return {
     "folke/todo-comments.nvim",
     opts = {
       highlight = {
-        pattern = [[.*<(KEYWORDS)\s*]], -- match keyword even without colon
+        -- pattern = [[.*<(KEYWORDS)\s*]], -- match keyword even without colon
         comments_only = false, -- also match outside of comments
       },
       search = {
-        pattern = [[\b(KEYWORDS)\b]], -- match keyword even without colon
+        -- pattern = [[\b(KEYWORDS)\b]], -- match keyword even without colon
       },
     },
   },
