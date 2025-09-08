19654fb64065a125f3c56a98430ff51bf002d13f
Claude Becker <becker@phys.ethz.ch>
2025-09-08 14:08:27 +0200

    neovim: custom diff colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,15 @@ return {
     lazy = true,
     opts = {
       style = "night",
+      -- custom colors for diff
+      on_colors = function(colors)
+        colors.diff = {
+          add = "#004200",
+          change = "#1f2231",
+          delete = "#420000",
+          text = "#2c809c",
+        }
+      end,
       on_highlights = function(hl, c)
         -- no special highlighting of inline code blocks
         hl["@markup.raw.markdown_inline"] = {
